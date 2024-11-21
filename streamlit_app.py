bb67663005cfaa46dc5ebaef0c93d40d4c307a09
bmix2 <72576557+bmix2@users.noreply.github.com>
2024-11-21 03:22:29 +0000

    center v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,7 +157,7 @@ def main():
         img = Image.open("Captura.PNG")
         st.image(img,use_column_width="True")
 
-        col1, col2, col3 = st.columns([1, 3, 1])
+        col1, col2, col3 = st.columns([1, 1, 1])
 
         with col2:
             st.title("🧑‍⚖️")
